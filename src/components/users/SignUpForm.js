f11164f8fa845eafd7d2d4b9b27f8ae5416d42df
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-17 11:50:04 +0100

    Added basic SignUpForm + install dependencies

--- src/components/users/SignUpForm.js
+++ src/components/users/SignUpForm.js
@@ -3,9 +3,25 @@ import React from "react";
 const SignUpForm = () => {
   return(
     <div>
+
       <form>
-        <p>Sing Up Form</p>
+        <p className="pb-4">Sing Up Form</p>
+
+        <div className="pb-4">
+          <label>Email</label>
+          <input type="email" required />
+        </div>
+
+        <div className="pb-4">
+          <label>Password</label>
+          <input type="password" required />
+          <p className="text-sm">Minimum length: 7 characters</p>
+        </div>
+
+        <button type="button">Sign up</button>
+
       </form>
+    
     </div>
   )
 };
