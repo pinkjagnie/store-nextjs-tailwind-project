fc2bd90f8ad402eb3e5d49555697845efa065a59
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-20 13:39:16 +0100

    Added logic to login user

--- src/components/users/LoginForm.js
+++ src/components/users/LoginForm.js
@@ -1,29 +1,75 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+import { signIn } from 'next-auth/react'
+import { useForm } from "react-hook-form";
 
 const LoginForm = () => {
+  const router = useRouter();
+
+  const { register, handleSubmit, reset, formState: { errors, isDirty, isValid } } = useForm({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  });
+
+  async function onSubmit(data) {
+    const enteredEmail = data.email;
+    const enteredPassword = data.password;
+
+    console.log(enteredEmail)
+    console.log(enteredPassword)
+
+    reset()
+
+    const result = await signIn('credentials', {
+      redirect: false,
+      email: enteredEmail,
+      password: enteredPassword,
+    });
+
+    if (!result.error) {
+      // set some auth state
+      router.replace('/');
+    }
+
+  }
 
   return(
     <div>
 
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <p className="pb-4">Login Form</p>
 
         <div className="pb-4">
           <label htmlFor='email' className="pr-6">Email</label>
-          <input type='email' id='email' />
+          <input type='email' id='email' {...register("email", { required: {
+            value: true,
+            message: 'This field is required'
+          }, pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "Please enter a valid email"
+          } })}/>
         </div>
 
         <div className="pb-6">
           <label htmlFor='password' className="pr-6">Password</label>
-          <input type='password' id='password' />
+          <input type='password' id='password' {...register("password", { required: {
+            value: true,
+            message: 'This field is required'
+          }, minLength: {
+            value: 7,
+            message: 'Minimum length: 7 characters' 
+          } })}/>
         </div>
 
         <div>
           <Link href='/signup' className='py-2 px-8 outline rounded-sm bg-slate-300 font-bold text-sm text-gray-800 cursor-pointer'>Create an account</Link>
         </div>
 
-        <button type="submit" className='outline mt-6 px-4 py-6 bg-slate-700 disabled:bg-gray-400 text-[whitesmoke]'>Login</button>
+        <button type="submit" className='outline mt-6 px-4 py-6 bg-slate-700 disabled:bg-gray-400 text-[whitesmoke]' disabled={!isDirty || !isValid}>Login</button>
 
       </form>
     
