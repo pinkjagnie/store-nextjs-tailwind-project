b9dc0b24cf02096f7de029090ea6778b4e765f96
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-20 11:37:17 +0100

    Added LoginPage

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LoginPage = () => {
+  return(
+    <section className="pt-20 text-center">
+      <h1 className="pb-10">Login below</h1>
+      
+    </section>
+  )
+};
+
+export default SignUpPage;
