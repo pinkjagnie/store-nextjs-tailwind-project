99a76b788dc739ff97ed6db318049b17d5efcc59
pinkjagnie <olga.krzysztofik@gmail.com>
2023-02-20 18:04:51 +0100

    Blocking Login&SignUp pages for the logged in user

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from 'next/router';
+
+import { getSession } from 'next-auth/react';
 
 import LoginForm from "@/components/users/LoginForm";
 
 const LoginPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (session) {
+        router.replace('/');
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, [router]);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  
   return(
     <section className="pt-20 text-center">
       <h1 className="pb-10">Login below</h1>
